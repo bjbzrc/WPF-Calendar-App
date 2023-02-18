e81c07991c3cfe8dcd703814d269e077fc77d8a2
Brandon Buttlar <bjbzrc@umsystem.edu>
2023-02-17 22:38:48 -0600

    Finished

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,79 +17,82 @@ namespace BrandonButtlarChallengeM5
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Made by Brandon Buttlar, SP 2023
     /// </summary>
     public partial class MainWindow : Window
     {
         public int currentCaret = 0;
-        public DateTime date1 = new DateTime(2023, 1, 1, 0, 0, 0);
+        public DateTime aptTime = new DateTime(2023, 1, 1, 0, 0, 0);
 
         public MainWindow()
         {
             InitializeComponent();
-            aptCal.SelectedDate = date1;
-            inputTime.Text = date1.ToString("hh:mm tt");
+            aptCal.SelectedDate = DateTime.Now; // avoids a possible null date
+            inputTime.Text = aptTime.ToString("hh:mm tt");
         }
 
-        private void setAptBtn(object sender, RoutedEventArgs e)
+        private void SetAptBtn(object sender, RoutedEventArgs e)
         {
-            DateTime curDate = new DateTime();
-            curDate = DateTime.Now;
-            int dayDiff = curDate.Day - date1.Day;
-            aptCal.SelectedDate = date1;
-            aptText.Text = $"Your appointment is on {aptCal.SelectedDate}. " +
-                $"It's {dayDiff} days from today.";
+            DateTime aptDate = new DateTime();
+            aptDate = (DateTime)aptCal.SelectedDate;
+            string formattedAptDate = aptDate.ToString("dddd, MMMM d, yyyy") + 
+                " at " + aptTime.ToString("hh:mm tt");
+            int dayDiff = aptDate.DayOfYear - DateTime.Now.DayOfYear;
+
+            if (dayDiff == 1) {
+                aptText.Text = $"Your appointment is on {formattedAptDate}. " +
+                    $"It's {dayDiff} day from today.";
+            }
+            if (dayDiff < 0)
+            {
+                aptText.Text = "Date selected is in the past, please pick a different date";
+            }
+            else
+            {
+                aptText.Text = $"Your appointment is on {formattedAptDate}. " +
+                    $"It's {dayDiff} days from today.";
+            }
         }
 
-        private void incrementTime(object sender, RoutedEventArgs e)
+        // Main method used to change the input time based on the user's
+        // cursor position in the input box.
+        private void IncrementTime(int direction)
         {
+            // We need to capture the caret position between button clicks
+            // b/c inputTime loses focus on a button press.
             currentCaret = inputTime.CaretIndex;
 
             switch (inputTime.CaretIndex)
             {
+                // Change hour val when caret is before ":"
                 case int i when i < 3:
-                    date1 = date1.AddHours(1);
-                    aptText.Text = $"Caret is {inputTime.CaretIndex}. time is = {date1.ToString("hh:mm tt")}";
+                    aptTime = aptTime.AddHours(direction);
                     break;
+                // Change minute val when caret is after ":" and before AM/PM
                 case int i when i >= 3 && i < 6:
-                    date1 = date1.AddMinutes(1);
-                    aptText.Text = $"Caret is {inputTime.CaretIndex}. time is = {date1.ToString("hh:mm tt")}";
+                    aptTime = aptTime.AddMinutes(direction);
                     break;
+                // Change AM/PM value of time
                 case int i when i >= 6:
-                    date1 = date1.AddHours(12);
-                    aptText.Text = $"Caret is {inputTime.CaretIndex}. time is = {date1.ToString("hh:mm tt")}";
+                    aptTime = aptTime.AddHours(12);
                     break;
                 default:
                     break;
             }
 
-            inputTime.Text = date1.ToString("hh:mm tt");
+            inputTime.Text = aptTime.ToString("hh:mm tt");
+            // Whenever inputTime.text is updated, we need to reassign the caret position.
             inputTime.CaretIndex = currentCaret;
         }
 
-        private void decrementTime(object sender, RoutedEventArgs e)
+        private void UpBtn(object sender, RoutedEventArgs e)
         {
-            currentCaret = inputTime.CaretIndex;
+            IncrementTime(1);
+        }
 
-            switch (inputTime.CaretIndex)
-            {
-                case int i when i < 3:
-                    date1 = date1.AddHours(-1);
-                    aptText.Text = $"Caret is {inputTime.CaretIndex}. time is = " + date1.ToString("hh:mm tt");
-                    break;
-                case int i when i >= 3 && i < 6:
-                    date1 = date1.AddMinutes(-1);
-                    aptText.Text = $"Caret is {inputTime.CaretIndex}. time is = " + date1.ToString("hh:mm tt");
-                    break;
-                case int i when i >= 6:
-                    date1 = date1.AddHours(-12);
-                    aptText.Text = $"Caret is {inputTime.CaretIndex}. time is = " + date1.ToString("hh:mm tt");
-                    break;
-                default:
-                    break;
-            }
-
-            inputTime.Text = date1.ToString("hh:mm tt");
-            inputTime.CaretIndex = currentCaret;
+        private void DownBtn(object sender, RoutedEventArgs e)
+        {
+            IncrementTime(-1);
         }
     }
 }
